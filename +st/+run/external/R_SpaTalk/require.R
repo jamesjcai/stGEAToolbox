529eea1cf8c95ff374d7ba83beb902846836ea2b
James Cai <jamescai@genomezoo.net>
2023-05-30 16:03:42 -0500

    .

--- +st/+run/external/R_SpaTalk/require.R
+++ +st/+run/external/R_SpaTalk/require.R
@@ -1,3 +1,9 @@
-install.packages(pkgs = 'devtools')
-devtools::install_github('linxihui/NNLM')
-devtools::install_github('ZJUFanLab/SpaTalk')
+if (!requireNamespace("devtools", quietly = TRUE)) {
+    install.packages("devtools", repo="http://cran.rstudio.com/")
+}
+if (!requireNamespace("NNLM", quietly = TRUE)) {
+    devtools::install_github("linxihui/NNLM")
+}
+if (!requireNamespace("SpaTalk", quietly = TRUE)) {
+    devtools::install_github("ZJUFanLab/SpaTalk")
+}
